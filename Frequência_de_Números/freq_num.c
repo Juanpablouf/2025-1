bfaf48def276f0c314aac2a84057a0b7e6d181f1
Juanpablouf <jupablosv123@gmail.com>
2025-05-02 21:52:28 -0300

    Implementa leitura dos números e inicialização do controle de frequência

---
+++ Frequência_de_Números/freq_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numero[10], freq[10];
+    int i, j, cont;
+
+    printf("Digite 10 numeros: \n");
+    for (i = 0; i < 10; i++){
+        scanf("%d",&numero[i]);
+        freq[i] = -1;
+    }
+    
+
+    return 0;
+}
