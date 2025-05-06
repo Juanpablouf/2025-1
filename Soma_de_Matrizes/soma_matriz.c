0f18047d0f942c39bc182ec9b70ca3d80055716d
Juanpablouf <jupablosv123@gmail.com>
2025-05-06 19:34:15 -0300

    Adiciona matrizes 3x3 e variável para soma

---
+++ Soma_de_Matrizes/soma_matriz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int matriz1[3][3] = {
+        {2, 2, 2},
+        {5, 5, 5},
+        {8, 8, 8} };
+
+    int matriz2[3][3] = {
+        {7, 7, 7},
+        {6, 6, 6},
+        {1, 1, 1} };
+
+    int resultado[3][3];
+    return 0;
+}
