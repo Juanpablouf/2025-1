78a289c52150e0acb3414000ee1e5c7834e13794
Juanpablouf <jupablosv123@gmail.com>
2025-05-06 21:26:28 -0300

    implementa entrada de dados para cadastro de alunos

--- Cadastro_de_Alunos/Cad_aluno.c
+++ Cadastro_de_Alunos/Cad_aluno.c
@@ -12,5 +12,17 @@ int main(){
     scanf("%d", &n);
     struct Aluno alunos[n];
 
+    for (int i = 0; i < n; i++) {
+        printf("\nCadastro do aluno %d\n", i + 1);
+        getchar();
+        printf("Nome: ");
+        fgets(alunos[i].nome, 100, stdin);
+        alunos[i].nome[strcspn(alunos[i].nome, "\n")] = 0;  
+        printf("Matrícula: ");
+        scanf("%d", &alunos[i].matricula);
+        printf("Média: ");
+        scanf("%f", &alunos[i].media); 
+    }
+
     return 0;
   }
