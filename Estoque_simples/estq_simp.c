8c8fd9eaeeb5acc6f1328b3420cd10adf03fe9f0
Juanpablouf <jupablosv123@gmail.com>
2025-05-07 15:03:34 -0300

    Implementa função para cadastrar produtos

--- Estoque_simples/estq_simp.c
+++ Estoque_simples/estq_simp.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 
+#define MAX_PRODUTOS 100
+
 typedef struct {
     char nome[50];
     int codigo;
     int quantidade;
     float preco;
 } Produto;
+
+void cadastrarProduto(Produto produtos[], int *total) {
+    if (*total >= MAX_PRODUTOS) {
+        printf("Limite de produtos atingido.\n");
+        return;
+    }
+    Produto novo;
+    printf("Nome: ");
+    scanf(" %[^\n]", novo.nome);
+    printf("Código: ");
+    scanf("%d", &novo.codigo);
+    printf("Quantidade: ");
+    scanf("%d", &novo.quantidade);
+    printf("Preço: ");
+    scanf("%f", &novo.preco);
+    produtos[*total] = novo;
+    (*total)++;
+    printf("Produto cadastrado com sucesso!\n");
+}
 
 int main(){
 
