ce113e4a20ded2ac33326d220486ea2c1b477ed2
Juanpablouf <jupablosv123@gmail.com>
2025-05-07 18:40:32 -0300

    Adiciona interface interativa com usuário

--- Fibonacci_recursivo/Fibo_rec.c
+++ Fibonacci_recursivo/Fibo_rec.c
@@ -8,6 +8,14 @@ int fibonacci(int n) {
 }
 
 int main(){
-    
+    int n;
+    printf("Digite um número: ");
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("Digite um número não negativo.\n");
+    } else {
+        int resultado = fibonacci(n);
+        printf("O %dº número de Fibonacci é: %d\n", n, resultado);
+    }
     return 0;
 }
